757fa0d1f3fd8dc4f4f796427a6ce79a10d372eb
Rodrigo Marineli Moreira <rodrigo.moreira@grupoaugustocury.com.br>
2018-02-28 16:18:37 -0300

    webpackstart

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import ImageMessage from './model/image-message.model';
+
+const m = new ImageMessage();
+
+console.log('not empty');
+console.log('Rodrigo');
+console.log('Marineli');
+console.dir(m);
